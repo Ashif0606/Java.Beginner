acff4ef634263c9e479b84b4f2342120b7764647
Ashif0606 <ashif.ahmed0606@gmail.com>
2017-03-03 20:06:49 -0500

    This is my second addition

---
+++ RevereseString.java
@@ -0,0 +1,26 @@
+package practicejava;
+
+public class RevereseString {
+
+	public RevereseString() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+//	String Ashif = "Ashif is he";
+//	String x[]=Ashif.split(" ");
+//	
+//	for (int j=x.length-1;j>=0;j--){
+//		System.out.print(x[j]+" ");
+//		 }
+//	
+		String Ashif = "Hey There";
+		char[] x=Ashif.toCharArray();
+		
+		for (int j=x.length-1;j>=0;j--){
+			System.out.print(x[j]+" ");
+			 }
+	 }
+	}
+
+
