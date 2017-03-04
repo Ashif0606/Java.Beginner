62f10eec9e766536ce680b6fb963725c7f3dbd33
Ashif0606 <Ashif.Ahmed0606@gmail.com>
2017-03-04 12:15:13 -0500

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package practicejava;
+
+public class Palindrome {
+
+	public Palindrome() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+//		int n =123;
+//		int i;
+//		int r=0;
+//		while(n>0){
+//			i=n%10;//3,2,1
+//			n=n/10;//12,1,0 because it's n is an (int)
+//			r=r*10+i;//3,32,321
+//	    }
+//		System.out.println(r);
+	
+	
+	
+	
+}}
